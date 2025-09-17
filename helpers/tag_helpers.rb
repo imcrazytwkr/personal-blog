9e7295ca51c1b8602d3baab4f8c4fd64beb4c262
Denis Chernov <cat.aka.xakep@gmail.com>
2025-09-17 16:21:29 +0300

    Optimised avatar on the main page and made it scaleable via a new helper

--- helpers/tag_helpers.rb
+++ helpers/tag_helpers.rb
@@ -16,24 +16,71 @@ module TagHelpers
     stylesheet_link_tag(*style_paths, **options)
   end
 
+  # A highly opinionated helper for scaled images. Initially written for
+  # main index page as inlining it into ERB is a nightmare
+  def vite_scaled_image_tag(path, params = {})
+    params.symbolize_keys!
+
+    max_scale = params.delete(:max_scale)
+
+    # Don't be evil
+    params.delete(:srcset)
+
+    raise ArgumentError.new(
+      "max_scale property is not defined for scaled image"
+    ) if max_scale.nil?
+
+    raise ArgumentError.new(
+      "max_scale property must be an integer"
+    ) unless max_scale.is_a? Integer
+
+    raise ArgumentError.new(
+      "max_scale has invalid value, must be at least 1"
+    ) if max_scale < 1
+
+    breadcrumbs = path.split(".")
+
+    extname = breadcrumbs.pop
+    basename = breadcrumbs.join(".")
+
+    if max_scale > 1
+      # While I could reuse the logic from vite_image_tag here, this
+      # implementation leaves the asset path mapping in a single place
+      # which simplifies maintenance
+      sources = (1..max_scale).map do |i|
+        "#{basename}_x#{i}.#{extname} #{i}x"
+      end
+
+      params[:srcset] = sources.join(", ")
+    end
+
+    vite_image_tag("#{basename}_x1.#{extname}", params)
+  end
+
   # Modified Middleman image_tag so that it finds the paths for srcset
   # in Vite manifest before passing them down to asset_path inside the
   # original image_tag helper
-  def vite_image_tag(path, params={})
+  def vite_image_tag(path, params = {})
     params.symbolize_keys!
 
     if params.key?(:srcset)
-      images_sources = params[:srcset].split(',')
+      images_sources = params[:srcset].split(",")
       images_sources.map! do |src_def|
-        if src_def.include? '//'
+        if src_def.include? "//"
           src_def.strip
         else
-          image_def, size_def = src_def.strip.split(/\s+/)
-          "#{vite_asset_path(:images, image_def)} #{size_def}"
+          image_def, size_def = src_def.strip.split(/\s+/, 2)
+
+          # @TODO: remove explicit scale append when fixed in middleman
+          #
+          # Explicitly appending the implicit "1x" scale value is necessary,
+          # because otherwise Middleman's `asset_path`` helper crashes even
+          # though such syntax is valid
+          "#{vite_asset_path(:images, image_def)} #{size_def || "1x"}"
         end
       end
 
-      params[:srcset] = images_sources.join(', ')
+      params[:srcset] = images_sources.join(", ")
     end
 
     image_tag vite_asset_path(:images, path), params
@@ -57,5 +104,4 @@ module TagHelpers
       vite_javascript_tag "theme-base", **options, type: "text/javascript"
     end
   end
-
 end
