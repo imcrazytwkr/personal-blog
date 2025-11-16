41c635531146f00877e5f08b3d0f87d6050d155b
Denis Chernov <cat.aka.xakep@gmail.com>
2025-11-16 15:26:13 +0200

    Added title and url helpers

--- helpers/nav_helpers.rb
+++ helpers/nav_helpers.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module NavHelpers
+  def site_url
+    data.meta.dig(:site, :absolute_url) || "http://localhost"
+  end
+
+  def absolute_url(relative_url)
+    URI.join(site_url, relative_url)
+  end
+
   def internal_resource(path)
     internal_path = "#{path.delete_prefix("/")}.html"
     sitemap.resources.find do |resource|
