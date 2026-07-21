1d5f556342dc67bc68afedf78b6c59d9f1a625b7
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-07-21 17:14:05 +0300

    Ported existing site content

---
+++ astro/src/plugins/rehype-image-class.js
@@ -0,0 +1,21 @@
+// Faithful port of Middleman's html_renderer.rb `image` callback, which
+// repurposed the markdown image `title` attribute as a CSS class:
+//   ![alt](src "max-w-full mx-auto")  ->  <img class="max-w-full mx-auto" ...>
+// Standard markdown would otherwise render that title verbatim as a tooltip.
+export default function rehypeImageClass() {
+  const walk = (node) => {
+    if (
+      node.type === 'element' &&
+      node.tagName === 'img' &&
+      node.properties &&
+      node.properties.title
+    ) {
+      node.properties.className = [String(node.properties.title)];
+      delete node.properties.title;
+    }
+    if (node.children) {
+      for (const child of node.children) walk(child);
+    }
+  };
+  return (tree) => walk(tree);
+}
