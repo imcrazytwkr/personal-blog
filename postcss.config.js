a392e0b9090a73dee7fdb7eb5a05f67f3046971d
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-07-21 13:47:21 +0300

    Cleaned-up redundant configs after migration to tailwind v4

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,8 @@
 import tailwind from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
-
-import tailwindConfig from './tailwind.config.js'
 
 // @SEE: https://github.com/vitejs/vite/issues/7808
 export default {
   plugins: [
-    tailwind(tailwindConfig),
-    autoprefixer,
+    tailwind(),
   ],
 };
