f5d68204594971f115c1b9b88b529568dd6e0764
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-07-21 22:59:41 +0300

    Implemented index page and post pagination

--- astro/src/content.config.ts
+++ astro/src/content.config.ts
@@ -1,5 +1,6 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
 
 const blog = defineCollection({
   loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
@@ -8,6 +9,7 @@ const blog = defineCollection({
     date: z.coerce.date(),
     tags: z.array(z.string()).default([]),
     author: z.string().optional(),
+    featured: z.boolean().optional(),
   }),
 });
 
