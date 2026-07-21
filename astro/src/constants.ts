521b37879911bdc32c63e32b543c6f1d421b9e85
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-07-21 22:21:22 +0300

    Typed colors.json

--- astro/src/constants.ts
+++ astro/src/constants.ts
@@ -12,19 +12,19 @@ export const AUTHOR = {
 
 export const BIO = `A personal blog by ${AUTHOR.name}`;
 
-export type SocialEntry = {
+export type SocialEntry = Readonly<{
   url: string;
   icon: string;
   alt: string;
   wide?: boolean;
-};
+}>;
 
-export const SOCIAL: SocialEntry[] = [
+export const SOCIAL = Object.freeze<SocialEntry[]>([
   { url: 'https://github.com/imcrazytwkr', icon: 'github.svg', alt: 'github' },
   { url: 'https://bitbucket.org/twkr/workspace/repositories', icon: 'bitbucket.svg', alt: 'bitbucket' },
   { url: 'https://linkedin.com/in/d-chernov', icon: 'linkedin.svg', alt: 'linkedin' },
   { url: 'https://hdev.im/@twkr', icon: 'mastodon.svg', alt: 'mastodon' },
-];
+]);
 
 export type MenuEntry = {
   name: string;
@@ -34,3 +34,10 @@ export type MenuEntry = {
 export const MENU: MenuEntry[] = [
   { name: 'About', url: '/about' },
 ];
+
+export const COLORS = Object.freeze<Record<string, string>>({
+  "linen": "#faf8f1",
+  "wheat": "#f8f5d7",
+  "gray": "#fbfbfb",
+  "light": "#ffffff"
+});
